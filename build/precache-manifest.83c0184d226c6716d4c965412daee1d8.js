4a5d8f9e75f2ca98fe121b3c4d239a309cd9e32f
Hoàng Phú <63839574+PhuNguyen01997@users.noreply.github.com>
2020-09-12 21:59:19 +0700

    First

---
+++ build/precache-manifest.83c0184d226c6716d4c965412daee1d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2fa5f59ab3b22b94bc815afdf52132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9f78cf8d847e05d9ae8",
+    "url": "/static/js/2.d9f4c1d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d9f4c1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b49100e2802d4c95a39",
+    "url": "/static/js/main.8d98a4c7.chunk.js"
+  },
+  {
+    "revision": "2868d4deac9eedcb439c",
+    "url": "/static/js/runtime-main.e12b78d6.js"
+  }
+]);
